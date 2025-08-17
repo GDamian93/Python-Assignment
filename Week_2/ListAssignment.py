7209b6814b7bf504c4204f9fa5ee3bfb66a3c892
GDamian93 <garciacia08@gmail.com>
2025-08-17 17:57:25 -0300

    Organize assignments by week

---
+++ Week_2/ListAssignment.py
@@ -0,0 +1,25 @@
+# **********  Week 2 Assignment **********
+
+# 1. Creates an empty List.
+my_list = []
+
+# 2. Appends elements to List.
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 3. Inserts an element at a specific position.
+my_list.insert(1, 15)
+
+# 4. Extends the list by appending elements from another list.
+my_list.extend([50, 60, 70])
+
+# 5. Removes the last element from the list.
+my_list.pop()
+
+# 6. Sorts the list in ascending order.
+my_list.sort()
+
+# 7. Finds the index of the first occurrence of a value.
+print(my_list.index(30))
